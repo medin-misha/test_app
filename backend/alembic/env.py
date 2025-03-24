6634e8206b8987b13a05cc6ba4f46553e63724d3
misha <medinskijmisa228@gmail.com>
2025-03-24 20:59:31 +0200

    migrations

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -18,10 +18,10 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from core import Base, settings
+target_metadata = Base.metadata
 
+config.set_main_option("sqlalchemy.url", settings.database_url)
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
