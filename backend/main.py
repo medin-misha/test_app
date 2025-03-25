93f933ff8d9d4bcaa1b6690494bf19f6b5e5bfeb
misha <medinskijmisa228@gmail.com>
2025-03-25 13:21:34 +0200

    update requirement and fix main

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
-from hadnlers import spending_router
+from hadnlers import spending_router, users_router
 
 
 app = FastAPI()
 
 app.include_router(spending_router)
-
+app.include_router(users_router)
 if __name__ == '__main__':
     uvicorn.run("main:app", reload=True, port=8000)
