4fbd7b6dc0d9626d4217b7908950e09ea1722cf4
misha <medinskijmisa228@gmail.com>
2025-03-25 06:39:29 +0200

    update init

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ("settings",)
+__all__ = ("settings", "Base", "User", "Spending", "db_helper")
 
 from .settings import settings
 from .models.base import Base
 from .models.user import User
 from .models.spending import Spending
+from .database import db_helper
