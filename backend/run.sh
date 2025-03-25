c62f5e1fb6f3237614947baacfbd02080845c73d
misha <medinskijmisa228@gmail.com>
2025-03-25 14:02:38 +0200

    add README and fix run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -3,8 +3,9 @@
 export postgres_host=postgres/postgres_db
 export postgres_user=postgres_user
 export postgres_password=postgres_password
-export currencylayer_key=твой api key от https://currencylayer.com/dashboard
+export currencylayer_key=8e20b54761b122c5f1af35d195a82292
+
 
 uv run alembic revision --autogenerate -m "migrations"
 uv run alembic upgrade head
-uv run gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+uv run gunicorn main:app --workers 33 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
